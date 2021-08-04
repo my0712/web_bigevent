af1eb5e97c323aa5d72f05b2c62f9b558bdcc728
张曼玉 <9201932+zhang_man_yu@user.noreply.gitee.com>
2021-08-04 16:38:21 +0800

    完成了主页功能的开发

---
+++ assets/js/index.js
@@ -0,0 +1,55 @@
+$(function(){
+    getUserInfo()
+   
+    var layer = layui.layer
+    // 点击按钮，实现退出功能
+    $('#btnLogout').on('click', function(){
+        layer.confirm('确定退出登录?', {icon: 3, title:'提示'}, function(index){
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            
+            layer.close(index);
+          });
+    })
+
+})
+
+// 获取用户基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function(res) {
+            if(res.status !==0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            // 调用 renderAvatar 渲染头像
+            renderAvatar(res.data)
+        },
+        // complete: function(res) {
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        //         localStorage.removeItem('token')
+        //         location.href = '/login.html'
+        //     }
+        // }
+    })
+}
+// 渲染用户头像
+function renderAvatar(user) {
+    // 1.获取用户的名称
+    var name = user.nickname || user.username
+    // 2.设置欢迎的文本
+    $('#welcome').html('欢迎&nbsp;&nbsp' + name)
+    // 3.按需渲染头像
+    if(user.user_pic !== null){
+        $('.layui-nav-img').arrt('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    }else {
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
